89fbdad350df1512c5fa6f5edc112cf80ce31e0b
chun-y <126945155+chun-y@users.noreply.github.com>
2023-05-26 00:06:44 +0800

    Add files via upload

---
+++ add.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add</title>
+    <link rel="stylesheet" href="add.css">
+</head>
+
+<body>
+       <h2>Add Course</h2>
+<div class=three> 
+        <label>Course name</label>
+        <br>
+        <input type="text" name="cos_cname" placeholder="Course name">
+        <br>
+        <label>Department name</label>
+        <br>
+        <input type="text" name="dep_cname" placeholder="Department name">
+        <br>
+        <label>Course type</label>
+        <br>
+        <input type="text" name="cos_type" placeholder="Course type">
+        <br>
+        <button type="submit">Submit</button>
+        <br>
+</div>
+</body>
+</html>
