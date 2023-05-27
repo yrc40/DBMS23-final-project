73b43afa2221a706f73569560ce2cf49b40a8d34
chun-y <126945155+chun-y@users.noreply.github.com>
2023-05-27 17:32:57 +0800

    Add files via upload

---
+++ Completed.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courses completed</title>
+    <link rel="stylesheet" href="Completed.css">
+</head>
+<body>
+   
+<div class =three>
+
+        <label>Courses completed</label>
+        <br>
+        <textarea name="completed_courses"
+                  style="font-size: 16px"
+                  rows="18"
+                  cols="20"
+                  placeholder="Courses completed"
+                  required></textarea>
+        <br>
+       
+        <br>
+        <button type="submit">Submit</button>
+        <br>
+        </div>
+</body>
+</html>
