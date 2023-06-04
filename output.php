a8ee2d6571424c8590429d0a86ea1c6f4e277a3b
chun-y <126945155+chun-y@users.noreply.github.com>
2023-06-04 21:31:43 +0800

    Add files via upload

---
+++ output.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML PUBLIC>
+<html>
+<head>
+<title>result</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+
+
+
+
+<body>
+        <style type="text/css">
+
+                #sitebody {
+                        width: 1500px;
+                        margin: 0 auto;
+                        font-size: 18px;
+                        
+                }
+
+                #sidebar_left {
+                        background-color: rgba(221, 185, 220, 0.84);
+                        width: 500px;
+                        height: 1200px;
+                        text-align: center;
+                        float: left;
+                }
+                #sidebar_right {
+                        background-color: rgba(221, 185, 220, 0.84);
+                        width: 500px;
+                        height: 1200px;
+                        text-align: center;
+                        float: right;
+                }
+                #content {
+                        margin-left: 500px;
+                        margin-right: 500px;
+                        height: 1200px;
+                        background-color:rgba(236, 207, 234, 0.84);
+                        text-align: center;
+                }
+
+        </style>
+
+
+        <div id="sitebody">
+                <div id="sidebar_left"><br>
+                         <h2>尚未修習必修</h2>
+                </div>
+                <div id="sidebar_right"><br>
+                         <h2>其他未滿足條件</h2>
+                </div>
+                <div id="content"><br>
+                         <h2>建議選課</h2>       
+                </div>
+
+        </div>
+
+
+</body>
+
+
+
+
+</html> 
