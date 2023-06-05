528123dd901bdf8edd183d3316715e266cfba823
yrc40 <yiruchen8266.mg10@nycu.edu.tw>
2023-06-05 22:07:46 +0800

    add index file

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+       <h2>歡迎使用電機系畢業預審比對系統</h2>
+<div class=three> 
+        <br>
+        <br>
+        <form action="Completed.php">
+        <input type="submit" value="加入已修習課程" />
+        </form>
+        <br>
+        <br>
+        <form action="add.php">
+        <input type="submit" value="新增未知課程" />
+        </form>
+        <br>
+        <br>
+        <form action="output.php">
+        <input type="submit" value="開始比對" />
+        </form>
+        <br>
+        <br>
+</div>
+</body>
+</html>
