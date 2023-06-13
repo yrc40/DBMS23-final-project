bd6895c6a756a4d2aa698ce70a8092cfbe0c21d8
mole9000 <105414399+mole9000@users.noreply.github.com>
2023-06-14 00:12:13 +0800

    Add files via upload

--- Completed.php
+++ Completed.php
@@ -33,35 +33,103 @@ include "db_connect.php"
         </form>
         <button type="submit" form="index">Home</button>
         </div>
+		
+		<?php
+			$course = "";
+			if (isset($_POST["completed_courses"])) {
+				$course = $_POST["completed_courses"];
+			}
 
-        <?php
-            $course="";
-            if(isset($_POST["completed_courses"])){
-                $course=$_POST["completed_courses"];
-            }
-            $course=explode("\n",$course);
-            for ($x=0; $x<count($course); $x++) {
-	            $acourse=preg_split("/\s+/", $course[$x]);
-                if(count($acourse)==9){
-                    $sql="INSERT INTO `my_course` (`rowid`, `cos_id`, `semester`, `dep_cname`, `cos_cname`, `cos_type`, `cos_credit`) 
-                        VALUES ('$acourse[0]','$acourse[1]','$acourse[2]','$acourse[3]','$acourse[4]','$acourse[5]','$acourse[6]')";
-                    if (mysqli_query($conn, $sql)) {
-                        echo "New record created successfully";
-                      } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                      }
-                }
-                else{
-                    $sql="INSERT INTO `my_course` (`rowid`, `cos_id`, `semester`, `dep_cname`, `cos_cname`, `cos_type`, `cos_credit`, `grade`, `grade_status`, `teacher`, `brief`) 
-                    VALUES ('$acourse[0]','$acourse[1]','$acourse[2]','$acourse[3]','$acourse[4]','$acourse[5]','$acourse[6]','$acourse[7]','$acourse[8]','$acourse[9]','$acourse[10]') ";
-                    if (mysqli_query($conn, $sql)) {
-                        echo "New record created successfully";
-                      } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                      }
-                }    
-            }
-            mysqli_close($conn);
-        ?>
+			$lines = explode("\n", $course);
+
+			echo gettype($lines) . "\n";
+			echo count($lines) . "\n";
+
+			$table_key = [];
+			$table_index = 0;
+			$table_value = [];
+
+			for ($i = 0; $i < count($lines); $i++) {
+				if (trim($lines[$i]) == '學期成績紀錄') {
+					$table_index = $i;
+					$table_key = explode(' ', $lines[$i + 1]);
+					break;
+				}
+			}
+
+			print_r($table_key);
+			echo "\n";
+
+			for ($i = $table_index + 2; $i < count($lines) - 5; $i++) {
+				$table_value[] = explode("\t", $lines[$i]);
+			}
+
+			echo count($table_value) . "\n";
+
+			// 連接到 MySQL 資料庫
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "ece";
+
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+			// 創建表格
+			$tableSql = "CREATE TABLE IF NOT EXISTS gradetable (
+				id INT AUTO_INCREMENT PRIMARY KEY,
+				學期 VARCHAR(10),
+				課程代碼 VARCHAR(10),
+				開課單位 VARCHAR(50),
+				課程名稱 VARCHAR(255),
+				選別 VARCHAR(10),
+				學分 DECIMAL(4, 2),
+				等級成績 VARCHAR(5),
+				成績狀態 VARCHAR(50),
+				任課老師 VARCHAR(50),
+				向度 VARCHAR(50)
+			)";
+
+			if ($conn->query($tableSql) === TRUE) {
+				echo "Table created successfully";
+			} else {
+				echo "Error creating table: " . $conn->error;
+			}
+			$truncateSql = "TRUNCATE TABLE gradetable";
+			$conn->query($truncateSql);
+
+			// 將資料插入表格
+			foreach ($table_value as $row) {
+				$semester = $row[1];
+				$courseId = $row[2];
+				$department = $row[3];
+				$courseName = $row[4];
+				$courseType = $row[5];
+				$credits = $row[6];
+				$grade = $row[7];
+				$gradeStatus = $row[8];
+				$instructor = $row[9];
+				$dimension = $row[10];
+
+				$insertSql = "INSERT INTO gradetable (學期, 課程代碼, 開課單位, 課程名稱, 選別, 學分, 等級成績, 成績狀態, 任課老師, 向度) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+				$stmt = $conn->prepare($insertSql);
+				$stmt->bind_param("ssssssssss", $semester, $courseId, $department, $courseName, $courseType, $credits, $grade, $gradeStatus, $instructor, $dimension);
+
+				if ($stmt->execute()) {
+					echo "Record inserted successfully!";
+				} else {
+					echo "Error inserting record: " . $stmt->error;
+				}
+			}
+
+			// 關閉與資料庫的連接
+			$conn->close();
+
+			echo "資料已插入到 SQL 資料庫中！\n";
+		?>
+
 </body>
 </html>
