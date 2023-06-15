687d0b2d97a3b0a1e0f847f23327d0547de0c53e
yrc40 <125744143+yrc40@users.noreply.github.com>
2023-06-15 20:18:18 +0800

    new add

--- add.php
+++ add.php
@@ -24,6 +24,7 @@ include "db_connect.php"
         <br>
         <label>Course credit</label>
         <br>
+        <!--restrict input value to handle exception-->
         <select name="cos_credit" required>
             <option>Course credit</option>
             <option>0</option>
@@ -35,12 +36,10 @@ include "db_connect.php"
             <option>6</option>
         </select>
         <br>
-        <!--<label>Course credit</label>
-        <br>
-        <input type="text" name="cos_credit" placeholder="Course credit">-->
         <br>
         <label>Course type</label>
         <br>
+        <!--restrict input value to handle exception-->
         <select name="cos_type" required>
             <option>Course type</option>
             <option>必修</option>
@@ -62,6 +61,14 @@ include "db_connect.php"
 </div>
 
 <?php
+
+function SaveChinese($string){
+    $pattern = '/[\p{Han}\p{Latin}]+/u'; 
+    preg_match_all($pattern, $string, $matches);
+    $filteredString = implode('', $matches[0]);
+    return $filteredString;
+}
+
 //define the variables
 $cos_cname = $_GET["cos_cname"];
 $cos_credit = $_GET["cos_credit"];
@@ -69,6 +76,8 @@ $cos_credit = (int)$cos_credit;
 $cos_type = $_GET["cos_type"];
 $liberal1101 = preg_match("/\領域課程/i", $cos_cname); //boolean
 $liberal1102 = preg_match("/\基本素養/i", $cos_cname); //boolean
+$PE1 = preg_match("/\體育/i", $cos_cname); //boolean
+$PE2 = preg_match("/\大一體育/i", $cos_cname); //boolean
 $bug = false;//boolean
 
 //insertion information
@@ -96,47 +105,68 @@ $rowid = mysqli_fetch_array($result)[0]+1;
 //input debug
 switch($cos_type) {
 case "通識":
-    if(!$liberal1101 && !$liberal1102){
+    if(!$liberal1101 && !$liberal1102) {
         echo "<script>alert('輸入的通識抵免向度可能不存在!請再輸入一次');</script>";
         $bug = true;
     } 
     break;
 case "體育":
-    if($cos_credit!=0){
+    if($cos_credit!=0) {
         echo "<script>alert('體育課程只能是0學分!請再輸入一次');</script>";
         $bug = true;
-    } 
+    } /*elseif(!$PE1 && !$PE2) {
+        echo "<script>alert('體育課程名稱可能有誤!請再輸入一次');</script>";
+        $bug = true;
+    }*/
     break;
 case "外語":
-    if($cos_credit==0){
+    if($cos_credit==0) {
         echo "<script>alert('外語課程不能是0學分!請再輸入一次');</script>";
         $bug = true;
     } 
     break;
 case "必修":
-    $sql = "SELECT COUNT(*) FROM `ece_course` WHERE (`main_class` = '基礎必修課程' OR `main_class` = '專業必修實驗課程') /*AND REGEXP_REPLACE(`cos_cname`, '[^[:alnum:]]', '')*/ AND `cos_cname`='$cos_cname'";
+    $cos_name = SaveChinese($cos_cname);
+    //echo $cos_name;
+    $sql = "SELECT * FROM `ece_course` WHERE (`main_class` = '基礎必修課程' OR `main_class` = '專業必修實驗課程') AND REGEXP_REPLACE(`cos_cname`, '[^[:alnum:]]', '') = '$cos_name' ";
     $result = $conn->query($sql);
-    print_r(mysqli_fetch_array($result));
+    $nums=mysqli_num_rows($result);
     debug($conn, $sql);
-    $result = $conn->query($sql);
-    if(mysqli_fetch_array($result)[0]==0){
-        echo "<script>alert('輸入之必修課程可能不是電機系必修!請再輸入一次');</script>";
+    if($nums==0) { 
+        echo "<script>alert('輸入之必修課程名稱可能有誤!請再輸入一次');</script>";
         $bug = true;
+    } else { //check credit
+        $sql = "SELECT `cos_credit` FROM (SELECT * FROM `1111course` UNION SELECT * FROM `1112course`) AS `111` WHERE REGEXP_REPLACE(`cos_cname`, '[^[:alnum:]]', '') = '$cos_name' AND `cos_type` = '必修'";
+        $result = mysqli_query($conn, $sql);
+        $credit = mysqli_fetch_array($result)[0];
+        print_r(mysqli_fetch_array($result)[0]);
+        if($credit != $cos_credit){
+            echo "<script>alert('必修課程$cos_cname 應為 $credit 學分!請再輸入一次');</script>";
+            $bug = true;
+        } 
     }
     break;
 }
 
 if(!$bug){
     if($cos_type == "通識") {
-        $sql = "INSERT INTO `my_course` (`rowid`, `cos_cname`,`cos_credit`, `cos_type`, `brief`) VALUES ('$rowid','$cos_cname','$cos_credit','$cos_type', '$cos_cname') ";
+        $sql = "INSERT INTO `my_course` (`rowid`, `cos_cname`,`cos_credit`, `cos_type`, `grade`, `grade_status`, `brief`) VALUES ('$rowid','$cos_cname','$cos_credit','$cos_type', '同意抵免', '已送註冊組', '$cos_cname') ";
         if (mysqli_query($conn, $sql)) {
             echo "insert successfully";
             liberal_alert($cos_credit, $cos_cname);
             } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
               }
+    } elseif($cos_type == "體育"){
+        $sql = "INSERT INTO `my_course` (`rowid`, `cos_cname`,`cos_credit`, `cos_type`, `grade`, `grade_status`,`brief`) VALUES ('$rowid','$cos_cname','$cos_credit','$cos_type', '同意抵免', '已送註冊組', '體育必修') ";
+        if (mysqli_query($conn, $sql)) {
+            echo "insert successfully";
+            cos_alert($cos_cname,$cos_credit, $cos_type);
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+              }
     } else {
-        $sql = "INSERT INTO `my_course` (`rowid`, `cos_cname`,`cos_credit`, `cos_type`) VALUES ('$rowid','$cos_cname','$cos_credit','$cos_type') ";
+        $sql = "INSERT INTO `my_course` (`rowid`, `cos_cname`,`cos_credit`, `grade`, `grade_status`, `cos_type`) VALUES ('$rowid','$cos_cname','$cos_credit', '同意抵免', '已送註冊組', '$cos_type') ";
         if (mysqli_query($conn, $sql)) {
              echo "insert successfully";
              cos_alert($cos_cname,$cos_credit, $cos_type);
